75b40ee1bbb9da90a4956ad72b679673b04711d3
BagusSatria123 <satriabagusputra123@gmail.com>
2020-10-26 08:38:53 +0700

    Chat bot

---
+++ application/views/admin/v_ChatBot.php
@@ -0,0 +1,23 @@
+<!-- Navigation -->
+<?php
+$this->load->view('admin/templates/v_header');
+$this->load->view('admin/templates/v_sidebar');
+?>
+
+<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
+    <div class="row">
+        <ol class="breadcrumb">
+            <li><a href="https://localhost/konveksi/welcome\">
+                    <em class="fa fa-home"></em>
+                </a></li>
+            <li class="active">Chat Bot</li>
+        </ol>
+    </div>
+    <!--/.row-->
+
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">Chat Bot</h1>
+        </div>
+    </div>
+    <!--/.row-->
